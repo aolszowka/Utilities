bbda40daed130b3ae9d55d720479d539be571d84
Ace Olszowka <aolszowka@gmail.com>
2015-03-05 16:11:00 -0700

    * Rough cut of a new utility program "DictionaryFilter" which is a way to dynamically create a Dictionary and then filter on the keys.

---
+++ DictionaryFilter/MainForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace DictionaryFilter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainerMain = new System.Windows.Forms.SplitContainer();
+            this.splitContainerInputs = new System.Windows.Forms.SplitContainer();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.menuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).BeginInit();
+            this.splitContainerMain.Panel1.SuspendLayout();
+            this.splitContainerMain.Panel2.SuspendLayout();
+            this.splitContainerMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerInputs)).BeginInit();
+            this.splitContainerInputs.Panel1.SuspendLayout();
+            this.splitContainerInputs.Panel2.SuspendLayout();
+            this.splitContainerInputs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Location = new System.Drawing.Point(0, 454);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.Size = new System.Drawing.Size(574, 22);
+            this.statusStripMain.TabIndex = 0;
+            this.statusStripMain.Text = "statusStrip1";
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(574, 24);
+            this.menuStripMain.TabIndex = 1;
+            this.menuStripMain.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // splitContainerMain
+            // 
+            this.splitContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerMain.Location = new System.Drawing.Point(0, 24);
+            this.splitContainerMain.Name = "splitContainerMain";
+            this.splitContainerMain.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainerMain.Panel1
+            // 
+            this.splitContainerMain.Panel1.Controls.Add(this.splitContainerInputs);
+            // 
+            // splitContainerMain.Panel2
+            // 
+            this.splitContainerMain.Panel2.Controls.Add(this.textBoxOutput);
+            this.splitContainerMain.Size = new System.Drawing.Size(574, 430);
+            this.splitContainerMain.SplitterDistance = 191;
+            this.splitContainerMain.TabIndex = 2;
+            // 
+            // splitContainerInputs
+            // 
+            this.splitContainerInputs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerInputs.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerInputs.Name = "splitContainerInputs";
+            // 
+            // splitContainerInputs.Panel1
+            // 
+            this.splitContainerInputs.Panel1.Controls.Add(this.textBoxInput);
+            // 
+            // splitContainerInputs.Panel2
+            // 
+            this.splitContainerInputs.Panel2.Controls.Add(this.textBoxFilter);
+            this.splitContainerInputs.Size = new System.Drawing.Size(574, 191);
+            this.splitContainerInputs.SplitterDistance = 286;
+            this.splitContainerInputs.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxInput.Location = new System.Drawing.Point(0, 0);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(286, 191);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.WordWrap = false;
+            this.textBoxInput.TextChanged += new System.EventHandler(this.OnInputsChanged);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxFilter.Location = new System.Drawing.Point(0, 0);
+            this.textBoxFilter.Multiline = true;
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFilter.Size = new System.Drawing.Size(284, 191);
+            this.textBoxFilter.TabIndex = 0;
+            this.textBoxFilter.WordWrap = false;
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.OnInputsChanged);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxOutput.Location = new System.Drawing.Point(0, 0);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(574, 235);
+            this.textBoxOutput.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(574, 476);
+            this.Controls.Add(this.splitContainerMain);
+            this.Controls.Add(this.statusStripMain);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Name = "MainForm";
+            this.Text = "Dictionary Filter";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.splitContainerMain.Panel1.ResumeLayout(false);
+            this.splitContainerMain.Panel2.ResumeLayout(false);
+            this.splitContainerMain.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerMain)).EndInit();
+            this.splitContainerMain.ResumeLayout(false);
+            this.splitContainerInputs.Panel1.ResumeLayout(false);
+            this.splitContainerInputs.Panel1.PerformLayout();
+            this.splitContainerInputs.Panel2.ResumeLayout(false);
+            this.splitContainerInputs.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerInputs)).EndInit();
+            this.splitContainerInputs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.SplitContainer splitContainerMain;
+        private System.Windows.Forms.SplitContainer splitContainerInputs;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.TextBox textBoxOutput;
+    }
+}
+
---
+++ DictionaryFilter/MainForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DictionaryFilter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// EventHander for when the Input TextBoxes Change.
+        /// </summary>
+        /// <param name="sender">The object that invoked this event.</param>
+        /// <param name="e">The arguments to this event.</param>
+        private void OnInputsChanged(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = "Processing...";
+
+            string output = _PerformDictionaryFilter(textBoxInput.Text, textBoxFilter.Text);
+
+            // Set our text
+            textBoxOutput.Text = output;
+        }
+
+        private static string _PerformDictionaryFilter(string inputDictionaryString, string filterString)
+        {
+            char delimiter = '\t';
+
+            IDictionary<string, string> inputDictionary = _ParseInputDictionaryString(inputDictionaryString, delimiter);
+            IEnumerable<string> filters = _ParseFilter(filterString);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string filter in filters)
+            {
+                if (inputDictionary.ContainsKey(filter))
+                {
+                    string value = inputDictionary[filter];
+
+                    // We don't have to include the delimiter, because
+                    // the "value" contains the original delimiter
+                    string reconstitutedValue = string.Format("{0}{1}", filter, value);
+                    sb.AppendLine(reconstitutedValue);
+                }
+                else
+                {
+                    string errorMessage = string.Format("Key {0} was not found. Processing has stopped.", filter);
+                    sb = new StringBuilder(errorMessage);
+                    break;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        ///     Given a "Dictionary String", which is defined as a string
+        /// that has the Dictionary KeyValuePairs delimited by the specified
+        /// delimiter and the entities delimited by NewLines. Parse or
+        /// "de-serialize" the string into a proper dictionary.
+        /// </summary>
+        /// <param name="inputDictionaryString">The "Dictionary String" to parse.</param>
+        /// <param name="delimiter">The KeyValuePair Delimiter.</param>
+        /// <returns>A Dictionary that represents the given "Dictionary String".</returns>
+        /// <remarks>
+        ///    Note the KeyValuePair delimiter will consider the first instance
+        /// of the delimiter to indicate the delineation. This means that any
+        /// additional instances of the delimiter within the entity (as
+        /// determined by NewLine) are considered part of the "Value".
+        /// </remarks>
+        private static IDictionary<string, string> _ParseInputDictionaryString(string inputDictionaryString, char delimiter)
+        {
+            IDictionary<string, string> returnDictionary = new Dictionary<string, string>();
+
+            // Split the input string on the new line character first
+            var dictionaryEntryStrings =
+                inputDictionaryString.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Now create the dictionary, currently the "Key" is assumed
+            // to be the value up until the first Tab, the remainder is
+            // the "Value"
+            foreach (string dictionaryEntryString in dictionaryEntryStrings)
+            {
+                Tuple<string, string> dictionaryEntry = _ParseDictionaryEntry(dictionaryEntryString, delimiter);
+
+                // Because this is user input we can't guarantee that we
+                // were given a legitimate dictionary we need to perform
+                // this check to make sure we don't have collisions.
+                if (returnDictionary.ContainsKey(dictionaryEntry.Item1))
+                {
+                    string errorMessage =
+                        string.Format("The Key `{0}` already existed in the input Dictionary", dictionaryEntry.Item1);
+                    throw new ArgumentException(errorMessage);
+                }
+                else
+                {
+                    returnDictionary.Add(dictionaryEntry.Item1, dictionaryEntry.Item2);
+                }
+            }
+
+            return returnDictionary;
+        }
+
+        /// <summary>
+        ///     Given a Dictionary Entry (A string that contains at least one
+        /// delimiter), split the string into the appropriate [Key,Value] pair.
+        /// </summary>
+        /// <param name="dictionaryEntryString">A string which represents a [Key,Value] pair delimited by the specified delimiter.</param>
+        /// <param name="delimiter">The specified delimiter.</param>
+        /// <returns>A Tuple that contains the [Key,Value] pair.</returns>
+        private static Tuple<string, string> _ParseDictionaryEntry(string dictionaryEntryString, char delimiter)
+        {
+            int delimiterIndex = dictionaryEntryString.IndexOf(delimiter);
+
+            if (delimiterIndex < 0)
+            {
+                string errorMessage =
+                    string.Format("The specified delimiter `{0}` was not found in entry `{1}`", delimiter, dictionaryEntryString);
+                throw new ArgumentException(errorMessage);
+            }
+
+            string key = dictionaryEntryString.Substring(0, delimiterIndex);
+            string value = dictionaryEntryString.Substring(delimiterIndex);
+
+            return new Tuple<string, string>(key, value);
+        }
+
+        /// <summary>
+        /// Given a NewLine delimited list of filters, return the unique filters as an IEnumerable.
+        /// </summary>
+        /// <param name="filterInputString">The filter string, delimited by NewLine.</param>
+        /// <returns>The distinct filters, in an Enumerable form.</returns>
+        private static IEnumerable<string> _ParseFilter(string filterInputString)
+        {
+            // Split the input string on the new line character first
+            var allFilters =
+                filterInputString.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            // We just want the distinct filters
+            var distinctFilters = allFilters.Distinct();
+
+            return distinctFilters;
+        }
+    }
+}
