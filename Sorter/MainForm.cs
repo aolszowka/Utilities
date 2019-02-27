a24e15f342bdfb1da03e50b6899cb7309d288ff5
Ace Olszowka <aolszowka@gmail.com>
2019-02-27 08:49:15 -0700

    Extracted existing logic to its own class to decouple the UI and BusinessLogic

--- Sorter/MainForm.cs
+++ Sorter/MainForm.cs
@@ -1,15 +1,12 @@
 ﻿// -----------------------------------------------------------------------
 // <copyright file="MainForm.cs" company="Ace Olszowka">
-// Copyright (c) Ace Olszowka 2014. All rights reserved.
+// Copyright (c) Ace Olszowka 2014-2019. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
 namespace Sorter
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Windows.Forms;
 
     /// <summary>
@@ -24,82 +21,6 @@ namespace Sorter
         public MainForm()
         {
             this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Given an "Input String" that is delimited by newline characters
-        /// </summary>
-        /// <param name="inputString">The "Input String" as described below.</param>
-        /// <param name="sortDescending">Indicates that the result should be returned in descending order.</param>
-        /// <param name="distinctInputs">Indicates that the result should only include the distinct elements.</param>
-        /// <returns>An "Output String" that is delimited by newline characters.</returns>
-        /// <remarks>
-        ///     The "Input String" and "Output String" are just a plain old
-        /// string that stores several inputs, delimited by a newline.
-        /// </remarks>
-        private static string _SortInput(string inputString, bool sortDescending, bool distinctInputs)
-        {
-            // TODO: This method needs to be re-factored and abstracted into its own library!
-
-            // Parse the Input String
-            IEnumerable<string> inputs = _ParseInputString(inputString);
-
-            // Perform the Actual Sorting/Distinct
-            IEnumerable<string> outputs = _SortInput(inputs, sortDescending, distinctInputs);
-
-            // Rebuild the output string
-            StringBuilder sb = new StringBuilder();
-            foreach (string output in outputs)
-            {
-                sb.AppendLine(output);
-            }
-
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// Given an Enumerable of strings, filter them according to the arguments.
-        /// </summary>
-        /// <param name="inputs">The input to filter.</param>
-        /// <param name="sortDescending">Indicates that the result should be returned in descending order.</param>
-        /// <param name="distinctInputs">Indicates that the result should only include the distinct elements.</param>
-        /// <returns>An Enumerable that is filtered according to the arguments.</returns>
-        private static IEnumerable<string> _SortInput(IEnumerable<string> inputs, bool sortDescending, bool distinctInputs)
-        {
-            IEnumerable<string> outputs = inputs;
-
-            // Determine if we should only return distinct
-            if (distinctInputs)
-            {
-                outputs = outputs.Distinct();
-            }
-
-            // Sort the inputs
-            if (sortDescending)
-            {
-                outputs = outputs.OrderByDescending(input => input);
-            }
-            else
-            {
-                outputs = outputs.OrderBy(input => input);
-            }
-
-            return outputs;
-        }
-
-        /// <summary>
-        /// Parses the "Input String" as defined below and then delineates it to a IEnumerable of strings.
-        /// </summary>
-        /// <param name="inputString">The "Input String" to parse.</param>
-        /// <returns>An Enumerable that represent the Inputs.</returns>
-        /// <remarks>
-        ///     The "Input String" is just a plain old string that stores
-        /// several inputs, delimited by a newline.
-        /// </remarks>
-        private static IEnumerable<string> _ParseInputString(string inputString)
-        {
-            return
-                inputString.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
         }
 
         /// <summary>
@@ -122,7 +43,7 @@ namespace Sorter
             this.textBoxOutput.Text = "Processing...";
 
             string output =
-                _SortInput(
+                SorterLogic.SortInput(
                 this.textBoxInput.Text,
                 this.descendingToolStripMenuItem.Checked,
                 this.distinctToolStripMenuItem.Checked);
---
+++ Sorter/SorterLogic.cs
@@ -0,0 +1,93 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MainForm.cs" company="Ace Olszowka">
+// Copyright (c) Ace Olszowka 2014-2019. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Sorter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
+    static class SorterLogic
+    {
+        /// <summary>
+        /// Given an "Input String" that is delimited by newline characters
+        /// </summary>
+        /// <param name="inputString">The "Input String" as described below.</param>
+        /// <param name="sortDescending">Indicates that the result should be returned in descending order.</param>
+        /// <param name="distinctInputs">Indicates that the result should only include the distinct elements.</param>
+        /// <returns>An "Output String" that is delimited by newline characters.</returns>
+        /// <remarks>
+        ///     The "Input String" and "Output String" are just a plain old
+        /// string that stores several inputs, delimited by a newline.
+        /// </remarks>
+        internal static string SortInput(string inputString, bool sortDescending, bool distinctInputs)
+        {
+            // TODO: This method needs to be re-factored and abstracted into its own library!
+
+            // Parse the Input String
+            IEnumerable<string> inputs = _ParseInputString(inputString);
+
+            // Perform the Actual Sorting/Distinct
+            IEnumerable<string> outputs = SortInput(inputs, sortDescending, distinctInputs);
+
+            // Rebuild the output string
+            StringBuilder sb = new StringBuilder();
+            foreach (string output in outputs)
+            {
+                sb.AppendLine(output);
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Given an Enumerable of strings, filter them according to the arguments.
+        /// </summary>
+        /// <param name="inputs">The input to filter.</param>
+        /// <param name="sortDescending">Indicates that the result should be returned in descending order.</param>
+        /// <param name="distinctInputs">Indicates that the result should only include the distinct elements.</param>
+        /// <returns>An Enumerable that is filtered according to the arguments.</returns>
+        internal static IEnumerable<string> SortInput(IEnumerable<string> inputs, bool sortDescending, bool distinctInputs)
+        {
+            IEnumerable<string> outputs = inputs;
+
+            // Determine if we should only return distinct
+            if (distinctInputs)
+            {
+                outputs = outputs.Distinct();
+            }
+
+            // Sort the inputs
+            if (sortDescending)
+            {
+                outputs = outputs.OrderByDescending(input => input);
+            }
+            else
+            {
+                outputs = outputs.OrderBy(input => input);
+            }
+
+            return outputs;
+        }
+
+        /// <summary>
+        /// Parses the "Input String" as defined below and then delineates it to a IEnumerable of strings.
+        /// </summary>
+        /// <param name="inputString">The "Input String" to parse.</param>
+        /// <returns>An Enumerable that represent the Inputs.</returns>
+        /// <remarks>
+        ///     The "Input String" is just a plain old string that stores
+        /// several inputs, delimited by a newline.
+        /// </remarks>
+        internal static IEnumerable<string> _ParseInputString(string inputString)
+        {
+            return
+                inputString.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
