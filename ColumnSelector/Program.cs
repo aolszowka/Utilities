960e1187166d9ed2bca3e0534b94df61ea640966
Ace Olszowka <aolszowka@gmail.com>
2022-05-27 13:50:43 -0600

    Initial Conversion

--- ColumnSelector/Program.cs
+++ ColumnSelector/Program.cs
@@ -21,6 +21,7 @@ namespace ColumnSelector
         public static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
