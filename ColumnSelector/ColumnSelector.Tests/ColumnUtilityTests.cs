dc7dd7b8571105b3a42b16e87c8ff3cb60d9883f
Ace Olszowka <aolszowka@gmail.com>
2022-05-27 14:01:35 -0600

    Split out test location

---
+++ ColumnSelector/ColumnSelector.Tests/ColumnUtilityTests.cs
@@ -0,0 +1,102 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ColumnUtilityTests.cs" company="Ace Olszowka">
+// Copyright (c) Ace Olszowka 2014. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ColumnSelector
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [TestFixture]
+    public class ColumnUtilityTests
+    {
+        /// <summary>
+        /// The default column delimiter (Tab)
+        /// </summary>
+        private const char DEFAULT_COLUMN_DELIMITER = '\t';
+
+        /// <summary>
+        /// The default column index (0)
+        /// </summary>
+        private const int DEFAULT_COLUMN_INDEX = 0;
+
+        #region GetColumnFromString
+
+        [Test]
+        public void GetColumnFromString_NullArgument_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromString(null, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX));
+        }
+
+        [Test]
+        public void GetColumnFromString_EmptyString_ArgumentException()
+        {
+            string testString = string.Empty;
+
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromString(testString, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX));
+        }
+
+        #endregion
+
+        #region GetColumnFromStrings
+
+        [Test]
+        public void GetColumnFromStrings_NullArgument_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromStrings(null, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX).ToArray());
+        }
+
+        [Test]
+        public void GetColumnFromStrings_EmptySet_ArgumentException()
+        {
+            IEnumerable<string> testSet = new string[1];
+
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromStrings(testSet, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX).ToArray());
+        }
+
+        #endregion
+
+        #region GetColumnFromStringSorted
+
+        [Test]
+        public void GetColumnFromStringSorted_NullArgument_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromStringSorted(null, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX));
+        }
+
+        [Test]
+        public void GetColumnFromStringSorted_EmptyString_ArgumentException()
+        {
+            string testString = string.Empty;
+
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromStringSorted(testString, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX));
+        }
+
+        #endregion
+
+        #region GetColumnFromStringsSorted
+
+        [Test]
+        public void GetColumnFromStringsSorted_NullArgument_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromStringsSorted(null, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX).ToArray());
+        }
+
+        [Test]
+        public void GetColumnFromStringsSorted_EmptySet_ArgumentException()
+        {
+            IEnumerable<string> testSet = new string[1];
+
+            Assert.Throws<ArgumentException>(() => ColumnUtility.GetColumnFromStringsSorted(testSet, DEFAULT_COLUMN_DELIMITER, DEFAULT_COLUMN_INDEX).ToArray());
+        }
+
+        #endregion
+    }
+}
---
+++ ColumnSelector/ColumnSelector.Windows/ColumnSelectorOptions.cs
@@ -0,0 +1,97 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ColumnSelectorOptions.cs" company="Ace Olszowka">
+// Copyright (c) Ace Olszowka 2014. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ColumnSelector
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class ColumnSelectorOptions : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Private backing store for the ColumnToSelect Property.
+        /// </summary>
+        private int columnToSelect;
+
+        /// <summary>
+        /// Private backing store for the ColumnDelimiter Property.
+        /// </summary>
+        private char columnDelimiter;
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets or sets the Column to Select.
+        /// </summary>
+        public int ColumnToSelect
+        {
+            get
+            {
+                return this.columnToSelect;
+            }
+
+            set
+            {
+                if (this.columnToSelect != value)
+                {
+                    this.columnToSelect = value;
+                    this.OnPropertyChanged("ColumnToSelect");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Column Delimiter.
+        /// </summary>
+        public char ColumnDelimiter
+        {
+            get
+            {
+                return this.columnDelimiter;
+            }
+
+            set
+            {
+                if (this.columnDelimiter != value)
+                {
+                    this.columnDelimiter = value;
+                    this.OnPropertyChanged("ColumnDelimiter");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method invoked on any property change.
+        /// </summary>
+        /// <param name="name">The name of the property changed.</param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        /// <summary>
+        /// Performs a Deep Copy of this Class.
+        /// </summary>
+        /// <returns>A deep copy of this instance.</returns>
+        public ColumnSelectorOptions Clone()
+        {
+            return new ColumnSelectorOptions()
+            {
+                ColumnToSelect = this.ColumnToSelect,
+                ColumnDelimiter = this.ColumnDelimiter
+            };
+        }
+    }
+}
