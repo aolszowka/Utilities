ec3a495233e8387f491dc31c67cca509cb64ea09
Ace Olszowka <aolszowka@gmail.com>
2022-05-27 15:54:13 -0500

    Upgrade Column Selector to .NET Core (#7)
    Upgrade Column Selector to .NET Core
    
    Split out the Unit Tests into their own Project in order to allow dotnet test and Visual Studio's Test Runner to properly run the unit tests. In doing this I restructured the code layout to reflect a Test Class vs the Windows Executable.
    
    I also improved the README and LICENSES

---
+++ ColumnSelector/ColumnSelector.Windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ColumnSelector")]
+[assembly: AssemblyDescription("Reads input and returns a particular column.\n Table Select Icon courtesy of FatCow (http://www.fatcow.com/free-icons Farm [Fresh Icons - Table select column icon])")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ace Olszowka")]
+[assembly: AssemblyProduct("ColumnSelector")]
+[assembly: AssemblyCopyright("Copyright © Ace Olszowka 2014-2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: SupportedOSPlatform("windows")] // Only Support Windows For Now
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e07fb290-e689-4c19-9640-e92d14c80dce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
--- ColumnSelector/README.md
+++ ColumnSelector/README.md
@@ -1,6 +1,26 @@
 # ColumnSelector
 A simple program to select a particular column of data.
 
+This program was written at a time I was dealing with a lot of tabular data that was being given to me in a tab delimited format. I had the need to extract certain columns of this data and was using Excel a lot to perform this. However I did not like how Excel would attempt to format data that was pasted into it. Therefore I created this utility program to perform this task.
+
+## Basic Usage
+Here is a GIF of this in action:
+
+![ColumnSelectorDemo](Docs/ColumnSelectorDemo.gif)
+
+The program is pretty straight forward; there are 2 "Panels" numbered in the following screenshot:
+
+![SorterPanelsNumbered](Docs/ColumnSelector.png)
+
+1. This is `Input`
+2. This is `Output`
+
+You can paste either a tab delimited plain text snippet or paste directly from Excel (or any rich text editor that has a grid) in the `Input` panel. Each input should be separated by a newline character for your operating system. Depending on the options chosen the `Output` panel will be the operation.
+
+Selecting `Options->Column Options` or using its hotkey (`CTRL+TAB`) will bring up a dialog that asks for the zero based index of the column to select (defaulting to `0`).
+
+This can also sort output and filter to distinct (case sensitive). The output dialogs can also be word wrapped when needed.
+
 ## Bugs/Feature Requests
 I accept pull requests and am responsive on GitHub, let me know!
 
